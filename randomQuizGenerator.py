9e413d0966188f03f593d86159b0381a6ef86e9b
Phil Prater <pprater92@gmail.com>
2019-03-21 16:18:48 -0700

    Added question writing, randomizing, and recording. Added answer key writing for each question.

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -32,10 +32,32 @@ for quizNum in range(35):
     # Write out header for the quiz.
     quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
     quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
-    quizFIle.write('\n\n')
+    quizFile.write('\n\n')
 
     # Shuffle the order of the states.
     states = list(capitals.keys())
     random.shuffle(states)
 
-    #TODO: loop through all 50 states, making a question for each.
+    #Loop through all 50 states, making a question for each.
+    for questionNum in range(50):
+        # Get right and wrong answers.
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+
+        # Write the question and answer options to the quiz file.
+        quizFIle.write('%s. What is the capital of %s?\n' % (questionNum + 1,
+        states[questionNum]))
+
+        for i in range(4):
+            quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+            quizFile.write('\n')
+
+            # Write answer key to file.
+            answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+
+    quizFile.close()
+    answerKeyFile.close()
