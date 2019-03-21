b0196624267faf3e224fd83ec801397e19d60f55
Phil Prater <pprater92@gmail.com>
2019-03-20 23:33:14 -0700

    Random quiz generator exercise.

---
+++ randomQuizGenerator.py
@@ -0,0 +1,19 @@
+# shebang?
+# creates random quizzes with Q/A in
+# random order, along with answer key.
+
+import random
+
+#Quiz data. Keys are states and values are capitals.
+capitals ={'Alabama': 'Mongomery', 'Alaska': 'Juneau', 
+        }
+
+
+for quizNum in range(35):
+    #TODO: create the quiz and answer key files.
+
+    #TODO: write out header for the quiz.
+
+    #TODO: shuffle the order of the states.
+
+    #TODO: loop through all 50 states, makeing a question for each.
